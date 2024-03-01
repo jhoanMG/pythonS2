e38af8b776978551b79633429f5594833fa7bb06
jhoanMG <135554986+jhoanMG@users.noreply.github.com>
2024-03-01 17:55:00 -0500

    cambio1

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
-nombre = "Juan"
-nombre =  45 #se cambia la variable, segun como la aignemos
+# Capturar información del primer colegio
+nombreC1 = input("Inserte el nombre del primer colegio: ")
+while True:
+    try:
+        estudiantes1 = int(input("Inserte el número de estudiantes en {}:".format(nombreC1)))
+        if estudiantes1 <= 0:
+            raise ValueError("El número de estudiantes debe ser mayor que cero.")
+        break
+    except ValueError:
+        print("Por favor, ingrese un número válido para el número de estudiantes.")
 
-print(type(nombre))
+# Capturar información del segundo colegio
+nombreC2 = input("Inserte el nombre del segundo colegio: ")
+while True:
+    try:
+        estudiantes2 = int(input("Inserte el número de estudiantes en {}:".format(nombreC2)))
+        if estudiantes2 <= 0:
+            raise ValueError("El número de estudiantes debe ser mayor que cero.")
+        break
+    except ValueError:
+        print("Por favor, ingrese un número válido para el número de estudiantes.")
 
-dato1 = 10
-dato2 = 20
-print(str(dato1)+str(dato2))
+# Imprimir la información de los colegios
+print("Nombre del primer colegio:", nombreC1)
+print("Estudiantes del colegio", nombreC1 + ":", estudiantes1)
+print("Nombre del segundo colegio:", nombreC2)
+print("Estudiantes del colegio", nombreC2 + ":", estudiantes2)
 
-#Anidar
-registro = True
-edad = 20
-print(type(registro)) #Inprimir el tipo de dato
-print(type(edad))
-
-if edad >= 18 and registro == True:
-    print("tiene permiso")
-else:
-    print("no tiene permiso")
-    if edad < 18:
-        print("no tiene permiso por ser menor de edad")
+# Calcular y mostrar el promedio
+promedio = (estudiantes1 + estudiantes2) / 2
+print("El promedio de estudiantes entre {} y {} es: {:.2f}".format(nombreC1, nombreC2, promedio))
